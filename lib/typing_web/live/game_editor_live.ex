b6c4fbd656da23c3454e8d0227d8075d5285c449
kohei0815 <kohei.s@thewaggle.co.jp>
2022-07-22 13:01:02 +0900

    main.html.heex、game_editor_live追記

--- lib/typing_web/live/game_editor_live.ex
+++ lib/typing_web/live/game_editor_live.ex
@@ -3,18 +3,21 @@ defmodule TypingWeb.GameEditorLive do
 
   def render(assigns), do: TypingWeb.GameEditorView.render(assigns.template, assigns)
 
-    # ~H"""
-    # <div>
-    # <h1><%= @page_title %></h1>
-    # </div>
-    # """
-
   def mount(_params, _session, socket) do
     socket =
       socket
+      |>assign(:input_key, "")
       |>assign(:page_title, "タイピングゲーム")
       |>assign(:template, "main.html")
 
     {:ok, socket}
+  end
+
+  def handle_event("toggle_input_key", params, socket) do
+   socket =
+    update(socket, :input_key, fn input_key ->
+      key =Map.get(params, "key", "")
+    end)
+    {:noreply, socket}
   end
 end
