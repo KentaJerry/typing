a660f0f450386c403a41849e15393eee08d37594
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2022-07-25 11:47:58 +0900

    演習2-3

--- lib/typing/editor/game_editor.ex
+++ lib/typing/editor/game_editor.ex
@@ -5,11 +5,34 @@ defmodule Typing.Editor.GameEditor do
     %__MODULE__{display_char: "HelloWorld"}
   end
 
-  def update(%__MODULE__{} = editor, "input_key", %{"key" => key}) do
+  @exclusion_key ~w(
+    Tab
+    Control
+    Shift
+    CapsLock
+    Alt
+    Meta
+    Eisu
+    KanjiMode
+    Backspace
+    Enter
+    Escape
+    ArrowLeft
+    ArrowRight
+    ArrowUp
+    ArrowDown
+  ) ++ [" "]
+
+  def update(%__MODULE__{} = editor, "input_key", %{"key" => key})
+    when key not in @exclusion_key do
     %{editor | input_char: editor.input_char <> key}
     # %{"key" => key} = params
     # input_char = editor.input_char
     # char = input_char <> key
     # %{editor | input_char: char}
+  end
+
+  def update(%__MODULE__{} = editor, "input_key", _params) do
+    editor
   end
 end
